6274537a1ed851f503ea19f2125a3e562cdf795f
Kminno <innocentmuwanda0@gmail.com>
2024-04-27 19:11:55 +0300

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,20 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Prep1 World!");
-        // Console.WriteLine("hey this is your first c# code");
-
-        // Console.Write("what is your name?");
-        // string name = Console.ReadLine();
-        // Console.WriteLine($"your name is {name}");
-
-        // int x = 5;
-        // int y = 10;
-
-        // if (x < y)
-        // {
-        //     Console.WriteLine("")
-        // }
 
         Console.WriteLine("What is your first name? ");
         string first_name = Console.ReadLine();
