ca0f471f6913191930b519e80c9a7f2a8ed01a47
Kminno <innocentmuwanda0@gmail.com>
2024-04-27 20:40:15 +0300

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magicalNumber= new Random();
+        int number = magicalNumber.Next(1, 11);
+        int guess = 0;
+
+        Console.WriteLine(" what is the maigical number between 1-11? ");
+
+        while (guess != number)
+        {
+            Console.Write("number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("it's Higer");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("it's Lower");
+            }
+            else
+            {
+                Console.WriteLine("Hoho! you guessed it");
+            }
+        }
+
     }
 }
