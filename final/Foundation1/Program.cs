f91d4079445edcfa1360bc1e9e764808f132908f
Muwanda Innocent <innocentmuwanda0@gmail.com>
2024-06-02 22:42:49 +0300

    week06

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,111 @@
 using System;
+using System.Collections.Generic;
 
+// Comment class
+class Comment
+{
+    // Attributes
+    private string _nameOfPerson;
+    private string _textOfComment;
+
+    // Constructor
+    public Comment(string nameOfPerson, string textOfComment)
+    {
+        _nameOfPerson = nameOfPerson;
+        _textOfComment = textOfComment;
+    }
+
+    // Method to represent the comment
+    public string CommentRepresentation()
+    {
+        return $"{_nameOfPerson}: {_textOfComment}";
+    }
+}
+
+// Video class
+class Video
+{
+    // Attributes
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _commentsList;
+
+    // Constructor
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _commentsList = new List<Comment>();
+    }
+
+    // Method to add a comment
+    public void AddComment(Comment comment)
+    {
+        _commentsList.Add(comment);
+    }
+
+    // Method to get the number of comments
+    public int NumberOfComments()
+    {
+        return _commentsList.Count;
+    }
+
+    // Method to display comments
+    public void DisplayComments()
+    {
+        foreach (var comment in _commentsList)
+        {
+            Console.WriteLine(comment.CommentRepresentation());
+        }
+    }
+
+    // Method to display video details
+    public void DisplayVideoDetails()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_length} seconds");
+        Console.WriteLine($"Number of Comments: {NumberOfComments()}");
+        Console.WriteLine("Comments:");
+        DisplayComments();
+        Console.WriteLine();
+    }
+}
+
+// Main Program class
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Creating video objects
+        Video video1 = new Video("How to make kikomando inn 2 mins", "Chef Ronnie", 300);
+        Video video2 = new Video("Understanding Luganda in 30days", "Lubuulwa Kenny", 1200);
+        Video video3 = new Video("Mpele for Beginners", "Dancer Ug", 1800);
+
+        // Adding comments to video1
+        video1.AddComment(new Comment("Jose", "That was da best!"));
+        video1.AddComment(new Comment("Abraham", "Its only two days but am already picking up ,great video."));
+        video1.AddComment(new Comment("Derrick", "Am soon startingg my own stall this is agreat vidoe for me"));
+
+        // Adding comments to video2
+        video2.AddComment(new Comment("Dave", "This is so helpful!"));
+        video2.AddComment(new Comment("Eva", "I learned a lot, thanks!"));
+        video2.AddComment(new Comment("Frank", "Amazing tutorial!"));
+
+        // Adding comments to video3
+        video3.AddComment(new Comment("Grace the dancing girl ug ", "Soon i will be showing off my new moves to my freinds."));
+        video3.AddComment(new Comment("Muzinyi", "Bro, can you make a nother video, i already have these down already, am rocking man."));
+        video3.AddComment(new Comment("kakole", "maan am rocking this side!"));
+
+        // Adding videos to a list
+        List<Video> videos = new List<Video> { video1, video2, video3 };
+
+        // Displaying video details
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,185 @@
 using System;
+using System.Collections.Generic;
 
+// Address class
+class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    // Constructor
+    public Address(string streetAddress, string city, string state, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _state = state;
+        _country = country;
+    }
+
+    // Method to detect if the address is in the USA
+    public bool IsInUSA()
+    {
+        return _country.ToLower() == "usa";
+    }
+
+    // Method to get the full address as a string
+    public string GetFullAddress()
+    {
+        return $"{_streetAddress}\n{_city}, {_state}\n{_country}";
+    }
+}
+
+// Customer class
+class Customer
+{
+    private string _customerName;
+    private Address _address;
+
+    // Constructor
+    public Customer(string customerName, Address address)
+    {
+        _customerName = customerName;
+        _address = address;
+    }
+
+    // Method to verify if the customer is in the USA
+    public bool IsInUSA()
+    {
+        return _address.IsInUSA();
+    }
+
+    // Method to get the customer's full address
+    public string GetAddress()
+    {
+        return _address.GetFullAddress();
+    }
+
+    // Getter for customer name
+    public string GetName()
+    {
+        return _customerName;
+    }
+}
+
+// Product class
+class Product
+{
+    private string _productName;
+    private int _productId;
+    private double _price;
+    private int _quantity;
+
+    // Constructor
+    public Product(string productName, int productId, double price, int quantity)
+    {
+        _productName = productName;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    // Method to compute the total cost of the product
+    public double ComputeTotalProductCost()
+    {
+        return _price * _quantity;
+    }
+
+    // Getter for product name
+    public string GetName()
+    {
+        return _productName;
+    }
+
+    // Getter for product ID
+    public int GetProductId()
+    {
+        return _productId;
+    }
+}
+
+// Order class
+class Order
+{
+    private List<Product> _listOfProducts;
+    private Customer _customer;
+
+    // Constructor
+    public Order(List<Product> products, Customer customer)
+    {
+        _listOfProducts = products;
+        _customer = customer;
+    }
+
+    // Method to calculate the total cost of the order
+    public double CalculateTotalCostOfOrder()
+    {
+        double totalCost = 0;
+        foreach (var product in _listOfProducts)
+        {
+            totalCost += product.ComputeTotalProductCost();
+        }
+        totalCost += CalculateShippingCost();
+        return totalCost;
+    }
+
+    // Method to calculate the shipping cost
+    private double CalculateShippingCost()
+    {
+        return _customer.IsInUSA() ? 5.0 : 35.0;
+    }
+
+    // Method to get the packing label string
+    public string PackingLabelString()
+    {
+        string packingLabel = "Packing Label:\n";
+        foreach (var product in _listOfProducts)
+        {
+            packingLabel += $"Product Name: {product.GetName()}, Product ID: {product.GetProductId()}\n";
+        }
+        return packingLabel;
+    }
+
+    // Method to get the shipping label string
+    public string ShippingLabelString()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress()}";
+    }
+}
+
+// Main program class
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Create some address objects
+        Address address1 = new Address("123 Maple St", "Springfield", "IL", "USA");
+        Address address2 = new Address("456 Elm St", "Vancouver", "BC", "Canada");
+
+        // Create some customer objects
+        Customer customer1 = new Customer("John Doe", address1);
+        Customer customer2 = new Customer("Jane Smith", address2);
+
+        // Create some product objects
+        Product product1 = new Product("Widget", 1001, 10.0, 2);
+        Product product2 = new Product("Gadget", 1002, 15.0, 1);
+        Product product3 = new Product("Doodad", 1003, 7.5, 3);
+        Product product4 = new Product("Thingamajig", 1004, 20.0, 1);
+
+        // Create some orders with products
+        Order order1 = new Order(new List<Product> { product1, product2 }, customer1);
+        Order order2 = new Order(new List<Product> { product3, product4 }, customer2);
+
+        // List of orders
+        List<Order> orders = new List<Order> { order1, order2 };
+
+        // Display details of each order
+        foreach (var order in orders)
+        {
+            Console.WriteLine(order.PackingLabelString());
+            Console.WriteLine(order.ShippingLabelString());
+            Console.WriteLine($"Total Cost: ${order.CalculateTotalCostOfOrder()}\n");
+        }
     }
 }
